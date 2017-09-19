703e3221a9ab1487a4e319507a16db2bbe480f61
MarcelJurtz <jurtzmarcel@gmail.com>
2017-09-19 21:11:04 +0200

    Fix sorting order of playercards

--- Kader.php
+++ Kader.php
@@ -1,120 +1,125 @@
-<html>
-<head>
-  <link rel="stylesheet" type="text/css" href="main_style.css"></link>  
+  <html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="main_style.css"></link>  
     <meta http-equiv="content-type" content="text/html; charset=utf-8">       
     <meta name="generator" content="PSPad editor, www.pspad.com">       
     <title> Kader </title> 
-</head>
+  </head>
 
-<!-- Beginn Body 
---------------- -->
-<body>
+  <!-- Beginn Body 
+    --------------- -->
+    <body>
 
-  <!-- Script Fancybox -->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+      <!-- Script Fancybox -->
+      <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 
-    <link rel="stylesheet" href="fancy/source/jquery.fancybox.css" type="text/css" media="screen" />
-    <script type="text/javascript" src="fancy/source/jquery.fancybox.pack.js"></script>
+      <link rel="stylesheet" href="fancy/source/jquery.fancybox.css" type="text/css" media="screen" />
+      <script type="text/javascript" src="fancy/source/jquery.fancybox.pack.js"></script>
 
-    <script type="text/javascript">
-  	$(document).ready(function() {
-		$(".fancybox").fancybox();
-  	});
+      <script type="text/javascript">
+       $(document).ready(function() {
+        $(".fancybox").fancybox();
+      });
     </script>
-    
-    
-<div class="container">
-<!-- Beginn container - schlie࠴ erst ganz am Ende -->
-  <!-- Beginn Header -->
-  <header>
-  
-    <div class ="banner">
-      <a href="index.php"> <img src="Galerie/banner_Logo.png"> </a>
-    </div>
 
-  </header>
-  <!-- ------------------------------------------------------------------------------------------------->
-  
-  <!-- Beginn Navigation -->
- <?php
- include ('menue.html');
- ?>
-  <!-- Navigation Ende ------------------------------------------------------------------------------->
-  
-  
-    <div class = "wrapper">
-        
-  <div class ="kader">
-   <div id ="saison_schrift">
-     Kader 2017
-     <hr>
-    </div> 
-     
-  <div class ="cards_galerie">
-  
-    <div class="manschaftsfoto"> 
-    <img src="Galerie/Playercards/teamfoto.jpg"> 
-    <hr>
-    </div>
-  
-   <div class ="reihe_1">
-    <ul>
-      <li><a class="fancybox" rel="group" href="Galerie/Playercards/marcohaas.jpg" title="Marco Haas 3B & P"> <img src="Galerie/Playercards/marcohaas_klein.jpg" alt="Marco Haas"> </a></li>
-      <li><a class="fancybox" rel="group" href="Galerie/Playercards/christophstienz.jpg" title="Christoph Stienz 1B"> <img src="Galerie/Playercards/christophstienz_klein.jpg" alt="Christoph Stienz"> </a></li>
-	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/carinadomke.jpg" title="Carina Domke 2B & RF"> <img src="Galerie/Playercards/carinadomke_klein.jpg" alt="Carina Domke"> </a></li>
-      <li><a class="fancybox" rel="group" href="Galerie/Playercards/arturkohlamai.jpg" title="Artur Kohlamai LF"> <img src="Galerie/Playercards/arturkohlamai_klein.jpg" alt="Artur Kohlamai"> </a></li>
-    </ul>
-    </div>
-	
-   <div class ="reihe_2">
-    <ul>
-	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/pascalspiess.jpg" title="Pascal Spieß"> <img src="Galerie/Playercards/pascalspiess_klein.jpg" alt="Pascal Spieß"> </a></li>
-	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/marvinschulz.jpg" title="Marvin Schulz RF"> <img src="Galerie/Playercards/marvinschulz_klein.jpg" alt="Marvin Schulz"> </a></li>
-	     <li><a class="fancybox" rel="group" href="Galerie/Playercards/romanschoen.jpg" title="Roman Sch&ouml;n C& CF" > <img src="Galerie/Playercards/romanschoen_klein.jpg" alt="Roman Sch&ouml;n"> </a></li>
-      <li><a class="fancybox" rel="group" href="Galerie/Playercards/mircowengrzic.jpg" title="Mirco Wengrzic"> <img src="Galerie/Playercards/mircowengrzic_klein.jpg" alt="Mirko Wengrzic"> </a></li>
-    </ul>
-   </div>
-   
-      <div class ="reihe_3">
+
+    <div class="container">
+      <!-- Beginn container - schlie࠴ erst ganz am Ende -->
+      <!-- Beginn Header -->
+      <header>
+
+        <div class ="banner">
+          <a href="index.php"> <img src="Galerie/banner_Logo.png"> </a>
+        </div>
+
+      </header>
+    <!-- ------------------------------------------------------------------------------------------------->
+    
+    <!-- Beginn Navigation -->
+    <?php
+    include ('menue.html');
+    ?>
+    <!-- Navigation Ende ------------------------------------------------------------------------------->
+    
+    
+      <div class = "wrapper">
+          
+    <div class ="kader">
+     <div id ="saison_schrift">
+       Kader 2017
+       <hr>
+      </div> 
+       
+    <div class ="cards_galerie">
+    
+      <div class="manschaftsfoto"> 
+      <img src="Galerie/Playercards/teamfoto.jpg"> 
+      <hr>
+      </div>
+    
+     <div class ="reihe_1">
       <ul>
-	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/christopweeber.jpg" title="Christop Weeber RF"> <img src="Galerie/Playercards/christopweeber_klein.jpg" alt="Christop Weeber"> </a></li>
-	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/christianlaechelt.jpg" title="Christian Laechelt 3B & P"> <img src="Galerie/Playercards/christianlaechelt_klein.jpg" alt="Christian Laechelt"> </a></li>
-      <li><a class="fancybox" rel="group" href="Galerie/Playercards/stefanjanezic.jpg" title="Stefan Janezic 2B"> <img src="Galerie/Playercards/stefanjanezic_klein.jpg" alt="Stefan Janezic"> </a></li>
-	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/michaelwaible.jpg" title="Michael Waible C"> <img src="Galerie/Playercards/michaelwaible_kl.jpg" alt="Michael Waible "> </a></li>  
-    </ul>
-   </div>
-   
-         <div class ="reihe_4">
-    <ul>
-	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/elmargoetz.jpg" title="Elmar G&ouml;tz"> <img src="Galerie/Playercards/elmargoetz_klein.jpg" alt="Elmar Goetz"> </a></li>  
-	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/georgschaefer.jpg" title="Georg Sch&auml;fer 1B & RF"> <img src="Galerie/Playercards/georgschaefer_klein.jpg" alt="Georg Sch&auml;fer"> </a></li>
-    </ul>
-   </div>
-  </div>
-  </div>
-  
-  
-  <!-- Beginn Footer ---------------------------------------------------------------------------------> 
-  <footer>
-  
-  <div class = "footer_box">
-    <div class=footer-text>
-    &copy; by Ravensburg Leprechauns 
+        <li><a class="fancybox" rel="group" href="Galerie/Playercards/marcohaas.jpg" title="Marco Haas 3B & P"> <img src="Galerie/Playercards/marcohaas_klein.jpg" alt="Marco Haas"> </a></li>
+        <li><a class="fancybox" rel="group" href="Galerie/Playercards/christophstienz.jpg" title="Christoph Stienz 1B"> <img src="Galerie/Playercards/christophstienz_klein.jpg" alt="Christoph Stienz"> </a></li>
+  	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/carinadomke.jpg" title="Carina Domke 2B & RF"> <img src="Galerie/Playercards/carinadomke_klein.jpg" alt="Carina Domke"> </a></li>
+        <li><a class="fancybox" rel="group" href="Galerie/Playercards/arturkohlamai.jpg" title="Artur Kohlamai LF"> <img src="Galerie/Playercards/arturkohlamai_klein.jpg" alt="Artur Kohlamai"> </a></li>
+      </ul>
+      </div>
+  	
+     <div class ="reihe_2">
+      <ul>
+  	   <li><a class="fancybox" rel="group" href="Galerie/Playercards/marvinschulz.jpg" title="Marvin Schulz RF"> <img src="Galerie/Playercards/marvinschulz_klein.jpg" alt="Marvin Schulz"> </a></li>
+        <li><a class="fancybox" rel="group" href="Galerie/Playercards/MoritzPechtl.jpg" title="Moritz Pechtl"> <img src="Galerie/Playercards/MoritzPechtl_klein.jpg" alt="Moritz Pechtl"> </a></li>
+        <li><a class="fancybox" rel="group" href="Galerie/Playercards/mircowengrzic.jpg" title="Mirco Wengrzic"> <img src="Galerie/Playercards/mircowengrzic_klein.jpg" alt="Mirko Wengrzic"> </a></li>
+  	   <li><a class="fancybox" rel="group" href="Galerie/Playercards/christopweeber.jpg" title="Christop Weeber RF"> <img src="Galerie/Playercards/christopweeber_klein.jpg" alt="Christop Weeber"> </a></li>
+      </ul>
+     </div>
+     
+        <div class ="reihe_3">
+        <ul>
+  	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/StefanGlaubitz.jpg" title="Coach Stefan Glaubitz"> <img src="Galerie/Playercards/StefanGlaubitz_klein.jpg" alt="Stefan Glaubitz"> </a></li>
+      <li><a class="fancybox" rel="group" href="Galerie/Playercards/christianlaechelt.jpg" title="Christian Laechelt 3B & P"> <img src="Galerie/Playercards/christianlaechelt_klein.jpg" alt="Christian Laechelt"> </a></li>
+        <li><a class="fancybox" rel="group" href="Galerie/Playercards/stefanjanezic.jpg" title="Stefan Janezic 2B"> <img src="Galerie/Playercards/stefanjanezic_klein.jpg" alt="Stefan Janezic"> </a></li>
+  	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/michaelwaible.jpg" title="Michael Waible C"> <img src="Galerie/Playercards/michaelwaible_kl.jpg" alt="Michael Waible "> </a></li>  
+      </ul>
+     </div>
+     
+           <div class ="reihe_4">
+      <ul>
+  	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/elmargoetz.jpg" title="Elmar G&ouml;tz"> <img src="Galerie/Playercards/elmargoetz_klein.jpg" alt="Elmar Goetz"> </a></li>  
+  	  <li><a class="fancybox" rel="group" href="Galerie/Playercards/georgschaefer.jpg" title="Georg Sch&auml;fer 1B & RF"> <img src="Galerie/Playercards/georgschaefer_klein.jpg" alt="Georg Sch&auml;fer"> </a></li>
+            
+              
+              
+          <li><a class="fancybox" rel="group" href="Galerie/Playercards/pascalspiess.jpg" title="Pascal Spieß"> <img src="Galerie/Playercards/pascalspiess_klein.jpg" alt="Pascal Spieß"> </a></li>
+          <li><a class="fancybox" rel="group" href="Galerie/Playercards/romanschoen.jpg" title="Roman Sch&ouml;n C& CF" > <img src="Galerie/Playercards/romanschoen_klein.jpg" alt="Roman Sch&ouml;n"> </a></li>
+      </ul>
+     </div>
+    </div>
     </div>
     
-    <div id="social"> 
-      <aside class="sidebar"> 
-      <tr>
-      <div class = "follow"> <td> <b> #follow us </b> </td> </div>
-      <td> <a href="https://www.facebook.com/RavensburgerLeprechauns/?fref=ts" target="_blank"> <img src="Galerie/Icons/icon-facebook.png"> </a>
-      <td> <a href="http://www.twitter.com/@RVLeps" target="_blank"> <img src="Galerie/Icons/icon-twitter.png"> </a>
-      </tr>
-      </aside>
+    
+    <!-- Beginn Footer ---------------------------------------------------------------------------------> 
+    <footer>
+    
+    <div class = "footer_box">
+      <div class=footer-text>
+      &copy; by Ravensburg Leprechauns 
+      </div>
+      
+      <div id="social"> 
+        <aside class="sidebar"> 
+        <tr>
+        <div class = "follow"> <td> <b> #follow us </b> </td> </div>
+        <td> <a href="https://www.facebook.com/RavensburgerLeprechauns/?fref=ts" target="_blank"> <img src="Galerie/Icons/icon-facebook.png"> </a>
+        <td> <a href="http://www.twitter.com/@RVLeps" target="_blank"> <img src="Galerie/Icons/icon-twitter.png"> </a>
+        </tr>
+        </aside>
+      </div>
     </div>
-  </div>
-    
-    
+      
+      
   </footer> <!-- Footer schlie࠴ -->
-  </div>  <!-- container schlie࠴ -->
-  </body> <!-- Body schlie࠴ -->
-  </html>
+</div>  <!-- container schlie࠴ -->
+</body> <!-- Body schlie࠴ -->
+</html>
