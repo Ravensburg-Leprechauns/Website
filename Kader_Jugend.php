d5852ddd37df4b9f43451f030accd79e1b13dead
MarcelJurtz <jurtzmarcel@gmail.com>
2017-09-21 22:15:25 +0200

    Add new playercards

---
+++ Kader_Jugend.php
@@ -0,0 +1,147 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="main_style.css"></link>  
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">       
+    <meta name="generator" content="PSPad editor, www.pspad.com">       
+    <title> Kader Jugend </title> 
+</head>
+
+<!-- Beginn Body 
+--------------- -->
+<body>
+
+  <!-- Script Fancybox -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+    <link rel="stylesheet" href="fancy/source/jquery.fancybox.css" type="text/css" media="screen" />
+    <script type="text/javascript" src="fancy/source/jquery.fancybox.pack.js"></script>
+
+    <script type="text/javascript">
+  	$(document).ready(function() {
+		$(".fancybox").fancybox();
+  	});
+    </script>
+    
+    
+<div class="container">
+<!-- Beginn container - schlie࠴ erst ganz am Ende -->
+  <!-- Beginn Header -->
+  <header>
+  
+    <div class ="banner">
+      <a href="index.php"> <img src="Galerie/banner_Logo.png"> </a>
+    </div>
+
+  </header>
+  <!-- ------------------------------------------------------------------------------------------------->
+  
+  <!-- Beginn Navigation -->
+ <?php
+ include ('menue.html');
+ ?>
+  <!-- Navigation Ende ------------------------------------------------------------------------------->
+  
+  
+    <div class = "wrapper">
+        
+  <div class ="kader">
+   <div id ="saison_schrift">
+     Kader Jugendmannschaft 2017
+     <hr>
+    </div> 
+     
+  <div class ="cards_galerie">
+  
+   <div class ="reihe_1">
+    <ul>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/SaskiaBlaha.jpg" title="Saskia Blaha">
+          <img src="Galerie/Playercards_Jugend/SaskiaBlaha_klein.jpg" alt="Saskia Blaha">
+        </a>
+      </li>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/JuliusMaucher.jpg" title="Julius Maucher">
+          <img src="Galerie/Playercards_Jugend/JuliusMaucher_klein.jpg" alt="Julius Maucher">
+        </a>
+      </li>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/SilvioMagino.jpg" title="Silvio Magino">
+          <img src="Galerie/Playercards_Jugend/SilvioMagino_klein.jpg" alt="Silvio Magino">
+        </a>
+      </li>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/LisaAlbrand.jpg" title="Lisa Albrand">
+          <img src="Galerie/Playercards_Jugend/LisaAlbrand_klein.jpg" alt="Lisa Albrand">
+        </a>
+      </li>
+    </ul>
+    </div>
+	
+   <div class ="reihe_2">
+    <ul>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/LeonSpiess.jpg" title="Leon Spieß">
+          <img src="Galerie/Playercards_Jugend/LeonSpiess_klein.jpg" alt="Leon Spieß">
+        </a>
+      </li>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/SusiAlbrand.jpg" title="Susi Albrand">
+          <img src="Galerie/Playercards_Jugend/SusiAlbrand_klein.jpg" alt="Susi Albrand">
+        </a>
+      </li>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/LeonaDietrich.jpg" title="Leona Dietrich">
+          <img src="Galerie/Playercards_Jugend/LeonaDietrich_klein.jpg" alt="Leona Dietrich">
+        </a>
+      </li>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/HendrikRau.jpg" title="Hendrik Rau">
+          <img src="Galerie/Playercards_Jugend/HendrikRau_klein.jpg" alt="Hendrik Rau">
+        </a>
+      </li>
+      </ul>
+      </div>
+      <div class="reihe_3">
+      <ul>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/AdrianWatzlawik.jpg" title="Adrian Watzlawik">
+          <img src="Galerie/Playercards_Jugend/AdrianWatzlawik_klein.jpg" alt="Adrian Watzlawik">
+        </a>
+      </li>
+      <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/LeonAbel.jpg" title="Leon Abel">
+          <img src="Galerie/Playercards_Jugend/LeonAbel_klein.jpg" alt="Leon Abel">
+        </a>
+      </li>
+	    <li>
+        <a class="fancybox" rel="group" href="Galerie/Playercards_Jugend/NicoSpiess.jpg" title="Nico Spieß">
+          <img src="Galerie/Playercards_Jugend/NicoSpiess_klein.jpg" alt="Nico Spieß">
+        </a>
+      </li>
+    </ul>
+  </div>
+   
+  <!-- Beginn Footer ---------------------------------------------------------------------------------> 
+  <footer>
+  
+  <div class = "footer_box">
+    <div class=footer-text>
+    &copy; by Ravensburg Leprechauns 
+    </div>
+    
+    <div id="social"> 
+      <aside class="sidebar"> 
+      <tr>
+      <div class = "follow"> <td> <b> #follow us </b> </td> </div>
+      <td> <a href="https://www.facebook.com/RavensburgerLeprechauns/?fref=ts" target="_blank"> <img src="Galerie/Icons/icon-facebook.png"> </a>
+      <td> <a href="http://www.twitter.com/@RVLeps" target="_blank"> <img src="Galerie/Icons/icon-twitter.png"> </a>
+      </tr>
+      </aside>
+    </div>
+  </div>
+    
+    
+  </footer> <!-- Footer schlie࠴ -->
+  </div>  <!-- container schlie࠴ -->
+  </body> <!-- Body schlie࠴ -->
+  </html>
